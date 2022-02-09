162bb78e9eb9842209472407f03e40d965b40768
jacobrugano <jacob.rugano@student.moringaschool.com>
2022-02-09 21:12:30 +0300

    Added the delete alert

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -29,7 +29,7 @@ completeGoal(isComplete, index){
 }
 deleteGoal(isComplete, index){
   if (isComplete) {
-    let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)
+    let toDelete = confirm(`Are you very sure you want to delete ${this.goals[index].name}?`)
 
     if (toDelete){
       this.goals.splice(index,1)
