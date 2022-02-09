ba227985312e35cdbe49173a1d89e99a4cf1d8c2
jacobrugano <jacob.rugano@student.moringaschool.com>
2022-02-09 21:14:17 +0300

    Added the date element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,4 @@ import { Goal } from './goal';
 })
 
 export class AppComponent {
-  // goals: Goal[] = [
-  //   {id:1, name:'Watch finding Nemo',description:'Find an online version and watch merlin find his son'},
-  //   {id:2,name:'Buy Cookies',description:'I have to buy cookies for the parrot'},
-  //   {id:3,name:'Get new Phone Case',description:'Diana has her birthday coming up soon'},
-  //   {id:4,name:'Get Dog Food',description:'Pupper likes expensive sancks'},
-  //   {id:5,name:'Solve math homework',description:'Damn Math'},
-  //   {id:6,name:'Plot my world domination plan',description:'Cause I am an evil overlord'},
-  // ];
 }
